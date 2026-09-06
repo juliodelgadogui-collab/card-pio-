4bb769b523a462e5bf09e6c45bfc680e7a0e00b4
juliodelgadogui-collab <juliodelgadogui@gmail.com>
2026-09-06 20:18:35 -0300

    Bump EventMenu GO app version

--- mobile/eventmenu-go/app/build.gradle.kts
+++ mobile/eventmenu-go/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "br.com.eventmenu.go"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
         buildConfigField("String", "API_BASE_URL", "\"$apiBase\"")
     }
 
