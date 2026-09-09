5a90e00301be3c6128898e60df172124c87e7a55
juliodelgadogui-collab <juliodelgadogui@gmail.com>
2026-09-09 07:29:32 -0300

    Gera QR do pareamento localmente no Windows

--- desktop/EventMenu.Desktop/HubPairingWindow.xaml.cs
+++ desktop/EventMenu.Desktop/HubPairingWindow.xaml.cs
@@ -1,5 +1,8 @@
+using System.IO;
 using System.Windows;
+using System.Windows.Media.Imaging;
 using EventMenu.Desktop.Models;
+using QRCoder;
 
 namespace EventMenu.Desktop;
 
@@ -13,6 +16,7 @@ public partial class HubPairingWindow : Window
         InitializeComponent();
         PairingCodeText.Text=pairing.Qr;
         ExpiresText.Text=FormatExpiry(pairing.ExpiresAt);
+        PairingQrImage.Source=CreateQr(pairing.Qr);
     }
 
     private void CopyButton_Click(object sender,RoutedEventArgs e)
@@ -22,6 +26,22 @@ public partial class HubPairingWindow : Window
     }
 
     private void CloseButton_Click(object sender,RoutedEventArgs e)=>Close();
+
+    private static BitmapSource CreateQr(string value)
+    {
+        using var generator=new QRCodeGenerator();
+        using var data=generator.CreateQrCode(value,QRCodeGenerator.ECCLevel.M);
+        var qr=new PngByteQRCode(data);
+        var bytes=qr.GetGraphic(12);
+        using var stream=new MemoryStream(bytes);
+        var image=new BitmapImage();
+        image.BeginInit();
+        image.CacheOption=BitmapCacheOption.OnLoad;
+        image.StreamSource=stream;
+        image.EndInit();
+        image.Freeze();
+        return image;
+    }
 
     private static string FormatExpiry(string value)
     {
