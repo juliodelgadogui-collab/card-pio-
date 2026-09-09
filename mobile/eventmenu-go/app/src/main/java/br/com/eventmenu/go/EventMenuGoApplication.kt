e16a33e458145907e2d861ea5e057161f23a0213
juliodelgadogui-collab <juliodelgadogui@gmail.com>
2026-09-09 18:58:46 -0300

    Registra Hub sem alterar GPS Premium

--- mobile/eventmenu-go/app/src/main/java/br/com/eventmenu/go/EventMenuGoApplication.kt
+++ mobile/eventmenu-go/app/src/main/java/br/com/eventmenu/go/EventMenuGoApplication.kt
@@ -24,8 +24,37 @@ class EventMenuGoApplication : Application() {
     lateinit var discountRepository:DiscountRepository;private set
     lateinit var cancellationRepository:CancellationRepository;private set
     lateinit var brandRepository:TenantBrandRepository;private set
+    lateinit var hubRepository:HubRepository;private set
     lateinit var pushCoordinator:FirebasePushCoordinator;private set
-    override fun onCreate(){super.onCreate();instance=this;val store=SecureSessionStore(this);ApiClient.configureSessionStore(store);ApiClient.configureOfflineCache(OfflineReadCache(this));val deviceId=DeviceIdentity.id(this);val baseUrl=BuildConfig.API_BASE_URL
-        repository=EventMenuRepository(baseUrl,deviceId,store);eventRepository=EventOperationsRepository(baseUrl,deviceId,store);eventBarRepository=EventBarRepository(baseUrl,deviceId,store);managerRepository=ManagerOperationsRepository(baseUrl,deviceId,store);notificationRepository=NotificationRepository(baseUrl,deviceId,store);receiptRepository=ReceiptRepository(baseUrl,deviceId,store);deviceStatusRepository=DeviceStatusRepository(baseUrl,deviceId,store);universalQrRepository=UniversalQrRepository(baseUrl,deviceId,store);tabSplitPaymentRepository=TabSplitPaymentRepository(baseUrl,deviceId,store);operatingUnitRepository=OperatingUnitRepository(baseUrl,deviceId,store);orderOperationsRepository=OrderOperationsRepository(baseUrl,deviceId,store);deliveryProgressRepository=DeliveryProgressRepository(baseUrl,deviceId,store);discountRepository=DiscountRepository(baseUrl,deviceId,store);cancellationRepository=CancellationRepository(baseUrl,deviceId,store);brandRepository=TenantBrandRepository(this,baseUrl,deviceId,store);pushCoordinator=FirebasePushCoordinator(this,notificationRepository,store);OperationNotificationScheduler.initialize(this);pushCoordinator.syncCurrentToken()}
+
+    override fun onCreate(){
+        super.onCreate()
+        instance=this
+        val store=SecureSessionStore(this)
+        ApiClient.configureSessionStore(store)
+        ApiClient.configureOfflineCache(OfflineReadCache(this))
+        val deviceId=DeviceIdentity.id(this)
+        val baseUrl=BuildConfig.API_BASE_URL
+        repository=EventMenuRepository(baseUrl,deviceId,store)
+        eventRepository=EventOperationsRepository(baseUrl,deviceId,store)
+        eventBarRepository=EventBarRepository(baseUrl,deviceId,store)
+        managerRepository=ManagerOperationsRepository(baseUrl,deviceId,store)
+        notificationRepository=NotificationRepository(baseUrl,deviceId,store)
+        receiptRepository=ReceiptRepository(baseUrl,deviceId,store)
+        deviceStatusRepository=DeviceStatusRepository(baseUrl,deviceId,store)
+        universalQrRepository=UniversalQrRepository(baseUrl,deviceId,store)
+        tabSplitPaymentRepository=TabSplitPaymentRepository(baseUrl,deviceId,store)
+        operatingUnitRepository=OperatingUnitRepository(baseUrl,deviceId,store)
+        orderOperationsRepository=OrderOperationsRepository(baseUrl,deviceId,store)
+        deliveryProgressRepository=DeliveryProgressRepository(baseUrl,deviceId,store)
+        discountRepository=DiscountRepository(baseUrl,deviceId,store)
+        cancellationRepository=CancellationRepository(baseUrl,deviceId,store)
+        brandRepository=TenantBrandRepository(this,baseUrl,deviceId,store)
+        hubRepository=HubRepository(baseUrl,deviceId,store)
+        pushCoordinator=FirebasePushCoordinator(this,notificationRepository,store)
+        OperationNotificationScheduler.initialize(this)
+        pushCoordinator.syncCurrentToken()
+    }
+
     companion object{lateinit var instance:EventMenuGoApplication;private set}
 }
